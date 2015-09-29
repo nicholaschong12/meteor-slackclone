67f545edbb59f32e396e150d33dca612430b957f
Nicholas <nicholaschong12@gmail.com>
2015-09-29 19:12:13 +0800

    Structure + 1st Part

---
+++ client/app.js
@@ -0,0 +1,25 @@
+Template.messages.helpers({
+  messages: [
+    { textt: "All these messages" },
+    { textt: "Uses the same template" },
+    { textt: "But have a different data context" },
+    { textt: "It's why these messages are all different!" },
+    { textt: "Hey!" },
+    { textt: "What's up man!" },
+    { textt: "Hello" }
+  ]
+});
+
+
+//Footer keypress
+Template.footer.events({
+	
+	'keypress input':function(event){
+		if(event.charCode == 13) {
+			event.stopPropagation();
+			$('.message-history').append('<div class="message"><a href="" class="message_profile-pic"></a><a href="" class="message_username">scotch</a><span class="message_timestamp">1:31 AM</span><span class="message_star"></span><span class="message_content">' + $('.input-box_text').val() + '</span></div>');
+			  $('.input-box_text').val("");
+            return false;
+		}
+	}
+});
