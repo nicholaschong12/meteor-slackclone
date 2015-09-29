e212f1c8737e334e6c88cb91db1016bce208886a
Nicholas <nicholaschong12@gmail.com>
2015-09-29 19:40:38 +0800

    Database

--- client/app.js
+++ client/app.js
@@ -1,25 +1,20 @@
 Template.messages.helpers({
-  messages: [
-    { textt: "All these messages" },
-    { textt: "Uses the same template" },
-    { textt: "But have a different data context" },
-    { textt: "It's why these messages are all different!" },
-    { textt: "Hey!" },
-    { textt: "What's up man!" },
-    { textt: "Hello" }
-  ]
+messages: Messages.find()
 });
 
 
 //Footer keypress
 Template.footer.events({
-	
-	'keypress input':function(event){
-		if(event.charCode == 13) {
-			event.stopPropagation();
-			$('.message-history').append('<div class="message"><a href="" class="message_profile-pic"></a><a href="" class="message_username">scotch</a><span class="message_timestamp">1:31 AM</span><span class="message_star"></span><span class="message_content">' + $('.input-box_text').val() + '</span></div>');
-			  $('.input-box_text').val("");
-            return false;
-		}
-	}
+  'keypress input': function(e) {
+    var inputVal = $('.input-box_text').val();
+    if(!!inputVal) {
+      var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
+      if (charCode == 13) {
+        e.stopPropagation();
+        Messages.insert({text: $('.input-box_text').val()});
+        $('.input-box_text').val("");
+        return false;
+      }    
+    }
+  }
 });
